4a581467763e3527ae147b43045eba538b1fc364
sangdongb <17743530214@163.com>
2021-05-25 19:25:20 +0800

    update 滚动图

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <m-header></m-header>
+  <tab></tab>
+  <router-view></router-view>
+</template>
+
+<script type="text/ecmascript-6">
+import MHeader from '@/components/m-header/m-header'
+import Tab from '@/components/tab/tab'
+
+export default {
+  components: {
+    MHeader,
+    Tab
+  }
+}
+</script>
